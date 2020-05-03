f0cada8d696ffe3e983f5efc03dc9d75a2245fe1
Colby Fayock <fayock@gmail.com>
2020-05-02 23:10:38 -0400

    Step 2: simulating loading state with content

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react';
+
 export default function Home() {
+  const [loadingState, updateLoadingState] = useState(true);
+  const [contentState, updateContentState] = useState({})
   const content = {
     header: `So, how 'bout them Knicks?`,
     intro: `What are their names? I'm Santa Claus! This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Good news, everyone! I've taught the toaster to feel love!`,
@@ -8,13 +12,19 @@ export default function Home() {
       `Who are those horrible orange men?`
     ]
   }
+  useEffect(() => {
+    setTimeout(() => {
+      updateContentState(content);
+      updateLoadingState(false)
+    }, 2000);
+  }, [])
   return (
     <div className="container">
       <main>
-        <h1>{ content.header }</h1>
-        <p>{ content.intro }</p>
+        <h1>{ contentState.header }</h1>
+        <p>{ contentState.intro }</p>
         <ul>
-          { content.list.map((item, i) => {
+          { Array.isArray(contentState.list) && contentState.list.map((item, i) => {
             return (
               <li>{ item }</li>
             )
